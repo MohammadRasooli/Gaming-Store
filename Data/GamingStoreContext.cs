8053657b6cc5f70352f4d41f001c1791fa14c694
Mohammad <MohammadRasooli.j@gmail.com>
2023-10-06 23:49:44 +0330

    Add data layer

---
+++ Data/GamingStoreContext.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gaming_Store.Data
+{
+    public class GamingStoreContext : DbContext
+    {
+        public GamingStoreContext(DbContextOptions<GamingStoreContext> options)
+             : base(options)
+        {
+            
+        }
+    }
+}
