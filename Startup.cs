873559eea8c0db6b388f9602d76c8be319a93f07
Mohammad <MohammadRasooli.j@gmail.com>
2023-10-08 01:34:04 +0330

    Add db context services

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Gaming_Store.Data;
+using Gaming_Store.Interfaces;
+using Gaming_Store.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +30,13 @@ namespace Gaming_Store
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GamingStoreContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("GamingStoreConnectionString")));
+
             services.AddControllers();
+
+            services.AddTransient<IProductRepository, ProductRepository>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
@@ -37,7 +47,7 @@ namespace Gaming_Store
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwagger();
-            
+
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("v1/swagger.json", "My API V1");
