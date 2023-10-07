ef25bf450ce674bd55e4dddafbf44bb393213ee7
Mohammad <MohammadRasooli.j@gmail.com>
2023-10-07 05:53:33 +0330

    Add product data

---
+++ Data/Product.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Gaming_Store.Data
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Discription { get; set; }
+
+        public double Price { get; set; }
+        
+        public int Inventory { get; set; }
+        
+        public string Image { get; set; }
+        
+        public int Category { get; set; }
+        
+        public DateTime CreationDate { get; set; }
+        
+        public DateTime UpdateDate { get; set; }
+        
+    }
+}
