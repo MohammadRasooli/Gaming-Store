94e6aa6036954bb2483c78f9269c4f3bcbc858d4
Mohammad <MohammadRasooli.j@gmail.com>
2023-11-02 18:10:53 +0330

    Added receiving all products

--- Interfaces/IProductRepository.cs
+++ Interfaces/IProductRepository.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gaming_Store.Models;
 
 namespace Gaming_Store.Interfaces
 {
     public interface IProductRepository
     {
-        
+        Task<List<ProductDetailsDto>> GetAllProducts();
     }
 }
