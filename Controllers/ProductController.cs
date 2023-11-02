951d27b7fb9626a6d2158f79d57b2e6aad45513b
Mohammad <MohammadRasooli.j@gmail.com>
2023-11-02 18:10:46 +0330

    Added receiving all products

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Gaming_Store.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Gaming_Store.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
         private readonly ILogger<ProductController> _logger;
+        private readonly IProductRepository _product;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController(ILogger<ProductController> logger,
+                                IProductRepository IProductRepository)
         {
             _logger = logger;
+            _product = IProductRepository;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var Products = await _product.GetAllProducts();
+            return Ok(Products);
         }
     }
 }
