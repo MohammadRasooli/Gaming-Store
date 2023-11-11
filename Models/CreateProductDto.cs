ac1d7c7a74f3f2df29b0c0c88b5185429e55e5b2
Mohammad <MohammadRasooli.j@gmail.com>
2023-11-11 23:53:12 +0330

    Added create product

---
+++ Models/CreateProductDto.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Gaming_Store.Models
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; }
+
+        public string Discription { get; set; }
+
+        public double Price { get; set; }
+
+        public int Inventory { get; set; }
+
+        public string Image { get; set; }
+
+        public int Category { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+    }
+}
